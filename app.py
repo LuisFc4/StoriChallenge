e97b5b2cb18095b3fe7c315b6374ce7ceb05e4bd
LuisFc4 <31329377+LuisFc4@users.noreply.github.com>
2022-07-29 01:06:20 -0500

    Create app.py

---
+++ app.py
@@ -0,0 +1,13 @@
+#libraries
+import pandas as pd 
+import numpy as np
+import datetime
+import matplotlib.pyplot as plt 
+import seaborn as sns
+import os
+import streamlit as st
+
+st.set_page_config(layout="wide")
+os.system("python -m pip install -r requirements.txt")
+
+st.title("Stori Data Challenge 2021")
