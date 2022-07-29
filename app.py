613cdefb4f49f9cd11b9fe9a233fbd480cfb75a4
LuisFc4 <31329377+LuisFc4@users.noreply.github.com>
2022-07-29 02:18:32 -0500

    Update app.py

--- app.py
+++ app.py
@@ -5,9 +5,57 @@ import datetime
 import matplotlib.pyplot as plt 
 import seaborn as sns
 import os
+import io
 import streamlit as st
 
 st.set_page_config(layout="wide")
 os.system("python -m pip install -r requirements.txt")
 
+
 st.title("Stori Data Challenge 2021")
+st.write("For this challenge we will use a fake credit card dataset that is attached as df.csv, which includes information from a public Kaggle dataset with three added fields: activated_date,last_payment_date and fraud.")
+
+
+df=df.drop(columns='Unnamed: 0')
+st.write(df)
+with st.container():
+    col_x, col_y ,col_z = st.columns(3)
+    with col_x:
+        st.write("Information")
+        buffer = io.StringIO()
+        df.info(buf=buffer)
+        s = buffer.getvalue()
+        st.text(s)
+    with col_y:
+        st.write("Describe")
+        st.write(df.describe())
+        
+    with col_z:
+        st.write("Nulls")
+        st.text(df.isnull().sum())
+
+question=st.selectbox("Question",["Question 1","Question 2","Question 3"])
+if question=="Question 1":
+    st.write("1.1 Plot an histogram of the balance amount for all the customers.")
+    st.write("1.2 Report any structure you find and any hypotheses you have about that structure.")
+    st.write("1.3 Report mean and median balance, grouped by year and month of activated_date.")
+    plt.title('Clients per balance')
+    plt.xlabel('Balance')
+    plt.ylabel('AMOUNT OF CLIENTS')
+    plt.hist(df.balance,bins=25, range=[0,20000])
+    st.write("Histogram of the balance amount for all the customers")
+    st.pyplot(plt)
+    st.write("Insights")
+    st.write("More than 70% of users have a balance below the average of 1565")
+    st.write("Another piece of information that immediately attracts attention is having so many nulls, especially in the minimum_payments column.")
+    st.write("Another data that caught my attention is activated_date since I see 13 null data but it may be because the activation has not yet been done")
+    st.write("Something that worries me is seeing only 15% of full payment but we should see the monthly targets to be able to compare it and think about how good or bad it is\n")
+    st.write("mean and median balance, grouped by year and month of activated_date")
+    mean = df.groupby([pd.DatetimeIndex(df['activated_date']).year,pd.DatetimeIndex(df['activated_date']).month])['balance'].mean()
+    median = df.groupby([pd.DatetimeIndex(df['activated_date']).year,pd.DatetimeIndex(df['activated_date']).month])['balance'].median()
+    st.write("################## Mean ##################")
+    st.write(mean)
+    st.write("################## Median ##################")
+    st.write(median)
+if question == "Question 2":
+    st.write("2.1 Report in a table the following information for customers who activated their account and made their last payment during 2020: cust_id (excluding letters), activated_date (in format YYYY-MM), last_payment_date (in format YYYY-MM-DD), cash_advance, credit_limit, and a calculated field of cash_advance as a percentage of credit_limit.")
